2482a32629db95a1335debbd4bd8033973ca6a39
Enrique Rubio <erubio@autentia.com>
2017-08-14 02:54:50 +0200

    visualizacion correcta del componente food-detail. Se añade circulo de progreso con los porcentajes de los macros. Se añade también openDialog al pulsar sobre el nombre del alimento.
    Cuando se guarda la nueva cantidad del alimento guardamos este en localstorage para que lo reciba el component home.

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -1,17 +1,19 @@
 import { FireService } from '../providers/fire.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Units } from './../data-model';
 import { DialogComponent } from './../dialog/dialog.component';
 import { MdDialog } from '@angular/material';
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { Food, Macros , units} from '../data-model';
+import { Food, Macros, units } from '../data-model';
 
 
 @Component({
   selector: 'app-food-detail',
-  templateUrl: './food-detail.component.html'
+  templateUrl: './food-detail.component.html',
+  styleUrls: ['./food-detail.component.css']
+
 })
-export class FoodDetailComponent implements OnInit, OnDestroy  {
+export class FoodDetailComponent implements OnInit, OnDestroy {
   food: Food;
   id: number;
   units = units;
@@ -19,28 +21,65 @@ export class FoodDetailComponent implements OnInit, OnDestroy  {
   todoList: any = [];
   okButtonText = 'Create task';
   sub: any;
-
+  color = 'primary';
+  mode = 'determinate';
+  porcentaje = { protein: 0, fat: 0, hc: 0 };
+  isDeterminate = true;
+  inicioProtein = '';
+  inicioFat = '';
+  hcGrades = 0;
+  proteinGrades = 0;
+  proteinHcSumGrades = 0;
   constructor(public dialog: MdDialog,
     private route: ActivatedRoute,
-    private fireService: FireService) {}
+    private router: Router,
+    private fireService: FireService) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-       this.id = +params['id']; // (+) converts string 'id' to a number
+      this.id = +params['id']; // (+) converts string 'id' to a number
 
-       // In a real app: dispatch action to load the details here.
+      // In a real app: dispatch action to load the details here.
     });
-    this.fireService.getFoodById(this.id).then(food => this.food = food);
-  }
+    this.fireService.getFoodById(this.id).then(food => {
+      this.food = food;
+      this.calculoPorcentaje();
 
-   openDialog(food) {
-    const dialogRef = this.dialog.open(DialogComponent, {data: food});
-    dialogRef.afterClosed().subscribe(result => {
-      this.selectedOption = result;
     });
   }
 
-   ngOnDestroy() {
+  openDialog(food) {
+    const dialogRef = this.dialog.open(DialogComponent, { data: food });
+    dialogRef.afterClosed().subscribe(foodDetail => {
+      this.selectedOption = foodDetail;
+      localStorage.setItem('foodDetail', JSON.stringify(food));
+    });
+  }
+
+  ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+  calculoPorcentaje() {
+    const fat = this.food.macros.fat;
+    const protein = this.food.macros.protein;
+    const hc = this.food.macros.hc;
+    const totalgrs = fat + protein + hc;
+    this.porcentaje.fat = Math.round(fat * 100 / totalgrs);
+    this.porcentaje.protein = Math.round(protein * 100 / totalgrs);
+    this.porcentaje.hc = Math.round(hc * 100 / totalgrs);
+
+    this.hcGrades = this.toGrades(this.porcentaje.hc);
+    this.proteinGrades = Math.round(this.toGrades(this.porcentaje.protein));
+
+    this.inicioProtein = 'rotate(' + this.hcGrades + 'deg)';
+    const proteinHcSum =  (this.porcentaje.protein + this.porcentaje.hc);
+    this.proteinHcSumGrades = Math.round(this.toGrades(proteinHcSum));
+    this.inicioFat = 'rotate(' + this.proteinHcSumGrades + 'deg)';
+  }
+  toGrades(porcentaje): number {
+    return porcentaje * 360 / 100;
+  }
+  getStartProtein(){
+    return 'transform: rotate(' + this.hcGrades + 'deg)';
   }
 }
