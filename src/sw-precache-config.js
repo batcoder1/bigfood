90a28b96abded3156e47ceb19ec4f928dc1ac456
Enrique Rubio <erubio@autentia.com>
2017-09-03 00:06:50 +0200

    pre-cache config

--- src/sw-precache-config.js
+++ src/sw-precache-config.js
@@ -1,14 +1,27 @@
-
 module.exports = {
-    staticFileGlobs: [
-      'dist/**.html',
-      'dist/**.js',
-      'dist/**.css',
-      'dist/assets/images/*',
-      'dist/assets/*'
-    ],
-    root: 'dist',
-    stripPrefix: 'dist/',
-    navigateFallback: '/index.html'
-     
-  };
+  staticFileGlobs: [
+    'dist/**.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/**.png',
+    'dist/vendor.**.bundle.js'
+  ],
+  root: 'dist',
+  stripPrefix: 'dist/',
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    origin: /fonts\.gstatic\.com/,
+      urlPattern: /(.*)$/,
+      handler: 'networkFirst',
+   
+    },
+    {
+    
+      urlPattern: /^(https\:)(\/\/)([^\:\/]*)(\:\d{1,5})?(\/)([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?([^\/\?\&\#\:]*)?\/?(.*)$/,
+      handler: 'networkFirst',
+   
+    }
+    
+    
+  ]
+};
