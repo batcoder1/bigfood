fcad2508cd31514780db90c1a9991a53e740d0ac
Enrique Rubio <erubio@autentia.com>
2017-09-03 00:09:37 +0200

    createUser, para cuando inicia por primera vez setUserData y getUserData para manejar la información del profile del user

--- src/app/providers/fire.service.ts
+++ src/app/providers/fire.service.ts
@@ -1,5 +1,5 @@
 import { forEach } from '@angular/router/src/utils/collection';
-import { Food, Meal, Profile, User, Units, Goals, DietDays } from './../data-model';
+import { Food, Meal, User, Units, Goals, DietDays } from './../data-model';
 import { Observable } from 'rxjs/Observable';
 import { Injectable, Input } from '@angular/core';
 import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
@@ -11,7 +11,6 @@ export class FireService {
   user: Observable<firebase.User>;
   foods: FirebaseListObservable<any[]>;
   afAuth: AngularFireAuth;
-  profile: Profile;
   fireUser: firebase.User;
   database: any;
   calendar: DietDays[];
@@ -21,91 +20,75 @@ export class FireService {
     this.foods = db.list('foods');
     this.afAuth = afAuth;
     this.database = firebase.database();
+    this.fireUser = JSON.parse(localStorage.getItem('fireUser'));
   }
 
   loginWithGoogle() {
-     return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
 
   logout() {
     return this.afAuth.auth.signOut();
   }
 
-  getUserProfile() {
+  getUser() {
     const database = firebase.database();
-    this.fireUser = JSON.parse(localStorage.getItem('fireUser'));
-
-    return firebase.database().ref('/profiles/' + this.fireUser.uid).once('value');
+    return this.database.ref('/users/' + this.fireUser.uid).once('value');
   }
+  getUserData() {
+    const database = firebase.database();
+    return this.database.ref('/profiles/' + this.fireUser.uid).once('value');
+  }
+  setUserData(fireUser: firebase.User, user: User) {
+    this.database.ref('profiles/' + fireUser.uid).set({
+      birthday: user.birthday,
+      email: user.email,
+      postalCode: user.postalCode,
+      country: user.country,
+      goals: user.goals,
+      units: user.units,
+      weight: user.weight,
+      height: user.height,
+      name: user.name
 
+    });
+  }
+  createUser(fireUser: firebase.User) {
+    return new Promise((resolve, reject) => {
+      this.database.ref('users/' + fireUser.uid).set({
+        email: fireUser.email,
+        profile_picture: fireUser.photoURL
+      });
+      resolve(0);
+    });
+  }
 
   getFoods() {
     return this.database.ref('/foods').once('value');
   }
-  getGoals() {
-    return this.database.ref('/goals').once('value');
-  }
-  getUserMeals() {
-    this.fireUser = JSON.parse(localStorage.getItem('fireUser'));
-    return this.database.ref('/meals/' + this.fireUser.uid).once('value');
-  }
+
   getUserDietDays() {
     this.fireUser = JSON.parse(localStorage.getItem('fireUser'));
     return this.database.ref('/dietDays/' + this.fireUser.uid).once('value');
+  }
+  setUserDietDays(userId, dietDays: DietDays[]) {
+    this.database.ref('dietDays/' + userId).set({
+      dietDays: dietDays
+    });
   }
 
   getFoodById(id): Promise<Food> {
     return new Promise((resolve, reject) => {
       this.getFoods().then(snapshot => {
         snapshot.forEach(child => {
-        const food = child.val();
-        if (food.id === id) {
-           resolve(food);
-        }
+          const food = child.val();
+          if (food.id === id) {
+            resolve(food);
+          }
         });
       });
     });
   }
-  setUserData(fireUser: firebase.User) {
-    this.database.ref('users/' + fireUser.uid).set({
-      email: fireUser.email,
-      profile_picture : fireUser.photoURL,
-    });
-  }
-  setUserProfile(userId, profile: Profile) {
-    this.database.ref('profiles/' + userId).set({
-      username: profile.username,
-      imageUrl: profile.imageUrl,
-      email: profile.email,
-      birthday : profile.birthday,
-      height : profile.height,
-      weight: profile.weight,
-      country: profile.country,
-      postalCode: profile.postalCode
-    });
-  }
 
-  setUserDietDays(userId, dietDays: DietDays[]) {
-    this.database.ref('dietDays/' + userId).set({
-         dietDays: dietDays
-    });
-  }
-  setUserGoals(userId, goals: Goals) {
-    this.database.ref('goals/' + userId).set({
-      initialWeight: goals.initialWeight,
-      currentWeight: goals.currentWeight,
-      desireWeight: goals.desireWeight,
-      weeklyGoal:  goals.weeklyGoal,
-      activityLevel: goals.activityLevel
-    });
-  }
- setUserUnits(userId, units: Units) {
-    this.database.ref('units/' + userId).set({
-      weight: units.weight,
-      height: units.height,
-      distance: units.distance,
-      Energy: units.Energy,
-      water: units.water
-    });
-  }
+
 }
