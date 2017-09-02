935a105c04ec2e113120b3c2c6fc2a133e1150c0
Enrique Rubio <erubio@autentia.com>
2017-09-03 00:08:20 +0200

    Dialog para editar los campos de profile

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,7 @@
-import { Goals, units, Units, User } from './../data-model';
+import { MdDialog } from '@angular/material';
+import { DialogComponent } from '../dialog/dialog.component';
+import { EventService } from '../providers/event.service';
+import { Goals, UNITS, COUNTRIES, Units, User } from './../data-model';
 import { FireService } from './../providers/fire.service';
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
@@ -10,11 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
   user: User;
-  goals: Goals;
+
   units: Units;
-  constructor(public fireService: FireService) {
-   fireService.getUserProfile().then(snapshot => this.user = snapshot.val());
-   fireService.getGoals().then(snapshot => this.goals = snapshot.val());
+  selectedOption: string;
+  countries = [];
+  myGoal: string;
+  goals = [
+    {id: 0, value: 'Perder 0,75 kg por semana'},
+    {id: 1, value: 'Perder 0,5 kg por semana'},
+    {id: 2, value: 'Perder 0,25 kg por semana'},
+    {id: 3, value: 'Mantener mi peso actual'},
+    {id: 4, value: 'Ganar 0,25 kg por semana'},
+    {id: 5, value: 'Ganar 0,5 kg por semana'}
+  ];
+  constructor(public fireService: FireService,
+    private eventService: EventService,
+    public dialog: MdDialog) {
+      this.countries = COUNTRIES;
+    // fireService.setUserData(JSON.parse(localStorage.getItem('fireUser')), this.user);
+    fireService.getUserData().then(snapshot => this.user = snapshot.val());
+  }
+  saveData() {
+    // this.valueEmitted.emit(value);
+    this.eventService.displayCancel(true);
+    this.eventService.displaySave(true);
+    localStorage.setItem('userData', JSON.stringify(this.user));
+  }
+
+  openDialog(label: string, user: User, nomvar: string) {
+    const dataUser = {
+      label : label,
+      value : user,
+      nomvar: nomvar
+    };
+    const dialogRef = this.dialog.open(DialogComponent, { data: dataUser });
+
+    dialogRef.afterClosed().subscribe(res => {
+      this.user = dataUser.value;
+      this.myGoal =  this.goals[dataUser.value.goals.weeklyGoal].value;
+    });
   }
 
   ngOnInit() {
